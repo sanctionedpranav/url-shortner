efab14976078df32222b819c115c9bdaa0127a87
sanctionedpranav <pranav9728@gmail.com>
2025-11-30 21:05:35 +0530

    all the sagrigation is done

--- backend/src/api/v1/routes/user-routes.js
+++ backend/src/api/v1/routes/user-routes.js
@@ -1,11 +1,12 @@
 import express from 'express'
+import { home, login, register } from '../../../controllers/user-controller.js';
 
 export const userRoutes = express.Router();
 
-userRoutes.get('/', (req, res) => {
-  res.send('<h1>Hello User</h1>');
-})
+userRoutes.get('/', home);
+userRoutes.post('/login', login);
+userRoutes.post('/register', register);
 
-userRoutes.get('/login', (req, res) => {
-  res.send('<h1>Login</h1>');
-})
+// userRoutes.get('/login', (req, res) => {
+//   res.send('<h1>Login</h1>');
+// })
---
+++ backend/src/controllers/user-controller.js
@@ -0,0 +1,11 @@
+export const home = (req, res) => {
+  res.send('<h1>URL shortner</h1>');
+}
+
+export const login = (req, res) => {
+  res.send('<h1>Login</h1>');
+}
+
+export const register = (req, res) => {
+  res.send('<h1>Register</h1>');
+}
